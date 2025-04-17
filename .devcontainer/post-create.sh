072a0d2de16c4f7697d6b2bf8094a557a8b646d5
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2025-04-17 06:28:57 +0000

    feat: add zsh plugin installation note

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,6 +7,7 @@ YELLOW="\033[0;33m"
 # zsh configurations
 # ------------------------------------------------------------------------------
 
+echo -e "${YELLOW}-> Install zsh plugins...${OFF}"
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
